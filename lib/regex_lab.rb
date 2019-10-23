ea85079a78d7f42e5643f3797da9ab10318f7214
Alex Martinez <alexandr3martin3z@gmail.com>
2019-10-23 05:42:52 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,12 +1,22 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+  if word.match(/[\A^aeiou]/)
+    true
+  else
+    false
+  end
+#binding.pry
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+  text.grep(/\Aun\zing/)
 
 end
 
 def words_five_letters_long(text)
+  text.grep(/^\w{5}\s/)
+
 
 end
 
